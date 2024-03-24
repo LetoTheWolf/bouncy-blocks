b3f1a6a2233aa2c28f93e9ea5958465446b8e301
LetoTheWolf <19koadam1451@gmail.com>
2024-03-23 21:04:22 -0400

    Initial Commit

--- src/main/java/net/leto/testingmod/TestingMod.java
+++ src/main/java/net/leto/testingmod/TestingMod.java
@@ -2,6 +2,7 @@ package net.leto.testingmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.leto.testingmod.item.ModItemGroups;
 import net.leto.testingmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,6 +13,7 @@ public class TestingMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		ModItemGroups.registerItemGroups();
 		LOGGER.info("Hello Fabric world!");
 		ModItems.registerModItems();
 	}
